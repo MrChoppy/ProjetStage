dd05690e6d4fb7b473a35dae4d2d829949549e0e
Mouad Malhoud <mouad-professionel@hotmail.com>
2023-11-05 02:01:20 -0500

    FAIT: ajout divider entre chaque etudiant dans candidatureEmployeur

--- lib/pages/candidaturesEmployeur.dart
+++ lib/pages/candidaturesEmployeur.dart
@@ -49,83 +49,107 @@ class _CandidaturesEmployeurState extends State<CandidaturesEmployeur> {
       appBar: AppBar(
         title: Text('Candidatures Employeur'),
       ),
-      body: Column(
-        children: [
-          DropdownButton<String>(
-            value: selectedStageId,
-            hint: Text('Sélectionner un stage'),
-            onChanged: (String? newStageId) {
-              setState(() {
-                selectedStageId = newStageId;
-              });
-            },
-            items: stages?.map((stage) {
-                  return DropdownMenuItem<String>(
-                    value: stage.id,
-                    child: Text(stage['poste'] as String),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, 
+          crossAxisAlignment:
+              CrossAxisAlignment.center, 
+          children: [
+            DropdownButton<String>(
+              value: selectedStageId,
+              hint: Text('Sélectionner un stage'),
+              onChanged: (String? newStageId) {
+                setState(() {
+                  selectedStageId = newStageId;
+                });
+              },
+              items: stages?.map((stage) {
+                    return DropdownMenuItem<String>(
+                      value: stage.id,
+                      child: Text(stage['poste'] as String),
+                    );
+                  }).toList() ??
+                  [],
+            ),
+            SizedBox(height: 16), 
+            FutureBuilder<QuerySnapshot>(
+              future: getCandidaturesForStage(selectedStageId ?? ''),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Erreur : ${snapshot.error}');
+                } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                  return Text('Aucune candidature trouvée pour ce stage.');
+                } else {
+                  print("Candidatures: ${snapshot.data!.docs.length}");
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        final candidatureData = snapshot.data!.docs[index]
+                            .data() as Map<String, dynamic>;
+                        print("Candidature $index: $candidatureData");
+
+                        final etudiantId =
+                            candidatureData['etudiantId'] as String;
+
+                        return FutureBuilder<Map<String, dynamic>>(
+                            future: getEtudiantInfo(etudiantId),
+                            builder: (context, etudiantSnapshot) {
+                              if (etudiantSnapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return CircularProgressIndicator();
+                              } else if (etudiantSnapshot.hasError) {
+                                return Text(
+                                    'Erreur : ${etudiantSnapshot.error}');
+                              } else {
+                                final etudiantInfo =
+                                    etudiantSnapshot.data ?? {};
+
+                                return Column(
+                                  children: [
+                                    ListTile(
+                                      title: Row(
+                                        children: [
+                                          Text(
+                                              'Nom de l\'étudiant: ${etudiantInfo['nom'] ?? 'N/A'}'),
+                                          SizedBox(width: 8),
+                                          Text(
+                                              '${etudiantInfo['prenom'] ?? 'N/A'}'),
+                                        ],
+                                      ),
+                                      subtitle: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                              'Email de l\'étudiant: ${etudiantInfo['email'] ?? 'N/A'}'),
+                                          Text(
+                                              'Remarques: ${etudiantInfo['remarques'] ?? 'N/A'}'),
+                                          Text(
+                                              'Date de postulation: ${candidatureData['dateCandidature'] ?? 'N/A'}'),
+                                        ],
+                                      ),
+                                    ),
+                                    if (index < snapshot.data!.docs.length - 1)
+                                      Divider(
+                                          thickness: 1,
+                                          color: Colors
+                                              .grey), 
+                                  ],
+                                );
+                              }
+                              ;
+                            });
+                      },
+                    ),
                   );
-                }).toList() ??
-                [],
-          ),
-          FutureBuilder<QuerySnapshot>(
-            future: getCandidaturesForStage(selectedStageId ?? ''),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              } else if (snapshot.hasError) {
-                return Text('Erreur : ${snapshot.error}');
-              } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                return Text('Aucune candidature trouvée pour ce stage.');
-              } else {
-                print("Candidatures: ${snapshot.data!.docs.length}");
-                return Expanded(
-                  child: ListView.builder(
-                    itemCount: snapshot.data!.docs.length,
-                    itemBuilder: (context, index) {
-                      final candidatureData = snapshot.data!.docs[index].data()
-                          as Map<String, dynamic>;
-                      print("Candidature $index: $candidatureData");
-
-                      // Récupérer l'ID de l'étudiant depuis la candidature
-                      final etudiantId =
-                          candidatureData['etudiantId'] as String;
-
-                      return FutureBuilder<Map<String, dynamic>>(
-                        future: getEtudiantInfo(etudiantId),
-                        builder: (context, etudiantSnapshot) {
-                          if (etudiantSnapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return CircularProgressIndicator();
-                          } else if (etudiantSnapshot.hasError) {
-                            return Text('Erreur : ${etudiantSnapshot.error}');
-                          } else {
-                            final etudiantInfo = etudiantSnapshot.data ?? {};
-
-                            return ListTile(
-                              title: Text(
-                                  'Nom de l\'étudiant: ${etudiantInfo['nom'] ?? 'N/A'}'),
-                              subtitle: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                      'Email de l\'étudiant: ${etudiantInfo['email'] ?? 'N/A'}'),
-                                  Text(
-                                      'Remarques: ${etudiantInfo['remarques'] ?? 'N/A'}'),
-                                  Text(
-                                      'Date de postulation: ${candidatureData['dateCandidature'] ?? 'N/A'}'),
-                                ],
-                              ),
-                            );
-                          }
-                        },
-                      );
-                    },
-                  ),
-                );
-              }
-            },
-          ),
-        ],
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
